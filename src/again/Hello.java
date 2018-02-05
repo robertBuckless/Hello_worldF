b76fd2598902e5e418852f15fdf451798ddb1af5
robertBuckless <robert.buckless@maine.edu>
2018-02-05 09:53:26 -0500

    new commit after save

--- src/again/Hello.java
+++ src/again/Hello.java
@@ -1,5 +1,9 @@
 package again;
 
 public class Hello {
-
+	public static void main(String[] args){
+		System.out.println("Hear ye: Important Anncouncement");
+		System.out.println("Hello World");
+		System.out.println("Good Bye");
+	}
 }
