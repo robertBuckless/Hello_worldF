1c01ea67e3e67e715602b8c9f9a4c4a4940f50a0
robertBuckless <robert.buckless@maine.edu>
2018-03-01 13:32:48 -0500

    Submitted Memory usage

--- src/again/Hello.java
+++ src/again/Hello.java
@@ -1,9 +1,14 @@
 package again;
 
+import java.text.DecimalFormat;
+
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hear ye: Important Anncouncement");
 		System.out.println("Hello World");
+		DecimalFormat df = new DecimalFormat("0.00");
+		Long freeMem = Runtime.getRuntime().freeMemory();
+		System.out.println(df.format(freeMem/1000000F) + "MB");
 		System.out.println("Good Bye");
 	}
 }
