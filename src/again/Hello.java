0cfaf504093fbe7b5ac7056dbd28b3ec57a6efe9
robertBuckless <robert.buckless@maine.edu>
2018-03-01 13:26:27 -0500

    Added date output

--- src/again/Hello.java
+++ src/again/Hello.java
@@ -1,9 +1,16 @@
 package again;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hear ye: Important Anncouncement");
 		System.out.println("Hello World");
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		Calendar cal = Calendar.getInstance();
+		System.out.println(dateFormat.format(cal.getTime()));
 		System.out.println("Good Bye");
 	}
 }
